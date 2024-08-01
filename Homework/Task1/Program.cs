c424b861e22a05a6e582397e93657cc7387d2819
Soulbring <lobakin_vova@vk.com>
2024-08-01 14:10:41 +0400

    Add Task 1

---
+++ Homework/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 55, 70, 10, 95, 55, 35, 6, 55, 75, 15 };
+int count = 0;
+int i = 0;
+int minRange = 9;
+int maxRange = 91;
+
+while (i < array.Length)
+{
+    if (array[i] > minRange && array[i] < maxRange) 
+    {
+        count++;
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+System.Console.WriteLine(count);
