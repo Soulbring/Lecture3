2f48d4417102a2340c89869de86f63b0f3dc85d9
Soulbring <lobakin_vova@vk.com>
2024-08-01 14:34:04 +0400

    Complete Task 2

--- Homework/Task2/Program.cs
+++ Homework/Task2/Program.cs
@@ -1,13 +1,57 @@
-﻿//Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+﻿using System;
+using System.Linq;
 
-int[] array = { 55, 70, 10, 94, 55, 30, 6, 55, 75, 15 };
-int count = 0;
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+        //Напишите свое решение здесь
+      int count = 0;
 
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < numbers.Length; i++)
 {
-    if (array[i] % 2 == 0)
+    if (numbers[i] % 2 == 0)
     {
         count++;
     }
 }
-System.Console.WriteLine(count);
+      return count;
+    }
+    
+    public static void PrintResult(int[] array)
+    {
+        //Напишите свое решение здесь
+      int count = CountEvenItems(array);
+      Console.WriteLine(count);
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
--- Homework/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Homework/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c424b861e22a05a6e582397e93657cc7387d2819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9227cb02693c896ceddaf6573c99fa8d48542a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
